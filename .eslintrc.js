d7ddd7807a35e2afc423efe9fed00e615d44d1af
Esteban Acevedo <astevenaa@gmail.com>
2023-12-23 12:53:03 -0700

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser for TypeScript
+  extends: [
+    'plugin:@typescript-eslint/recommended', // TypeScript rules
+    'plugin:prettier/recommended', // Prettier recommended rules
+  ],
+  plugins: [
+    '@typescript-eslint', // TypeScript plugin
+    'graphql',
+    'prettier',
+  ],
+  rules: {
+    'graphql/template-strings': [
+      'error',
+      {
+        env: 'literal',
+        schemaJson: require('./path/to/your/schema.json'),
+      },
+    ],
+    // ... any other TypeScript or JavaScript rules you might want to add
+  },
+  // ... any other ESLint configuration settings
+}
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+
+const typeDefs = `#graphql
+    type Product {
+    name: String
+    price: Float
+    quantity: Int
+    tags: [String]
+}
+type Query {
+  getProduct(id: String): Product
+}
+  `
+
+const resolvers = {
+  Query: {
+    async getProduct() {
+      return { name: 'Copper' }
+    },
+  },
+}
+ 
+const server = new ApolloServer({
+  typeDefs, //typeDef -> definig our Graphql types ( product, query, mutation)
+  resolvers, // resolver -> to create logic for certain graphql types(queary, mutation)
+})
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+})
+
+console.log('server running on ' + url)
