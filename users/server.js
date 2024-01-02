7a3de5c65f0aa815f77861d1bf58d9057b67a6ed
Esteban Acevedo <astevenaa@gmail.com>
2024-01-01 23:32:32 -0700

    test

--- users/server.js
+++ users/server.js
@@ -2,7 +2,12 @@ import { Express } from 'express'
 import { graphqlHTTP as expressGraphQL } from 'express-graphql'
 
 const app = express()
-
+app.use(
+  '/graphql',
+  expressGraphQL({
+    graphql: true,
+  })
+)
 app.listen(4000, () => {
   console.log('Listening')
 })
