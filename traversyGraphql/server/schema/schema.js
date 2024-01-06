2bb0f2726619423a05a93af3b38fa2dc81f0cb81
Esteban Acevedo <astevenaa@gmail.com>
2024-01-05 22:39:57 -0700

    schema

--- traversyGraphql/server/schema/schema.js
+++ traversyGraphql/server/schema/schema.js
@@ -7,6 +7,7 @@ const {
   GraphQLInt,
   GraphQLNonNull,
   GraphQLID,
+  graphql,
 } = require("graphql");
 
 const ClientType = new GraphQLObjectType({
@@ -25,9 +26,48 @@ const ClientType = new GraphQLObjectType({
   }),
 });
 
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    // projects: {
+    //   type: new GraphQLList(ProjectType),
+    //   resolve(parent, args) {
+    //     return projects.filter((project) => project.clientId === parent.id);
+    //   },
+    // },
+  }),
+});
+
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    //getting all clients
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects;
+      },
+    },
+    //getting a single client
+    project: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find((project) => project.id === args.id);
+      },
+    },
+    //getting all clients
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return clients;
+      },
+    },
+    //getting a single client
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
@@ -39,5 +79,5 @@ const RootQuery = new GraphQLObjectType({
 });
 
 module.exports = new GraphQLSchema({
-    query: RootQuery,
+  query: RootQuery,
 });
