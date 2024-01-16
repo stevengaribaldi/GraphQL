5edda00e50aeaa0e21a272723cbc09a237eaf9fd
Esteban Acevedo <astevenaa@gmail.com>
2024-01-16 15:43:37 -0700

    file push

---
+++ vite-project/cybpress.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  projectId: 'j4smn4',
+  // ...rest of the Cypress project config
+}
---
+++ vite-project/test/stack.test.js
@@ -0,0 +1,34 @@
+class Stack {
+  constructor() {
+    this.top = -1
+    this.items = {}
+  }
+
+  get peek() {
+    return this.items[this.top]
+    }
+    push(value) {
+        this.top += 1
+        this.items[this.top] = value
+    }   
+}
+
+describe('My Stack', () => {
+  let stack
+  beforeEach(() => {
+    stack = new Stack()
+  })
+
+  it('is created empty', () => {
+    const stack = new Stack()
+    expect(stack.top).toBe(-1)
+    expect(stack.items).toEqual({})
+  })
+
+  it('can push to the top ', () => {
+    stack.push('avocado')
+    expect(stack.top).toBe(0)
+    expect(stack.peek).toBe('avocado')
+  })
+  //   it('can pop off')
+})
