686dbbb076885fe7c31bff9d9739cd8813f2483f
Esteban Acevedo <astevenaa@gmail.com>
2024-01-18 17:50:23 -0700

    push

--- traversyGraphql/server/config/db.js
+++ traversyGraphql/server/config/db.js
@@ -3,12 +3,11 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-    //   // these are just to get rid of some deprecation warnings
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    //   useFindAndModify: false,
+      //   // these are just to get rid of some deprecation warnings
     });
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log(
+      `MongoDB connected: ${conn.connection.host}`.cyan.underline.bold
+    );
   } catch (err) {
     console.log(err);
     process.exit(1);
